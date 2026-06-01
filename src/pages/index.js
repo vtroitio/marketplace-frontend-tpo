2015b3b6561d21b064860f2ccb5f6859fc292155
Tobias <totobias2002@gmail.com>
2026-06-01 18:45:54 -0300

    feat: wrap paragraph tags in pages (Home, Register, Login)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { ComponentsPage } from "./ComponentsPage";
 export { HomePage } from "./HomePage";
+export { LoginPage } from "./LoginPage";
+export { RegisterPage } from "./RegisterPage";
 export { NotFoundPage } from "./NotFoundPage";
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { MainLayout } from "../components/layout";
 import { Cart } from "../pages/Cart";
 import { Checkout } from "../pages/Checkout";
-import { ComponentsPage, HomePage, NotFoundPage } from "../pages";
+import { ComponentsPage, HomePage, LoginPage, RegisterPage, NotFoundPage } from "../pages";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<ComponentsPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
       <Route element={<MainLayout />}>
         <Route path="/home" element={<HomePage />} />
